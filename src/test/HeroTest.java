0461c286cadf6b7f36158f9ae8c881d1c7a0f684
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-17 15:30:47 +0300

    pass the test instantiated hero name

--- src/test/HeroTest.java
+++ src/test/HeroTest.java
@@ -2,8 +2,14 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
+    @Test 
     public void hero_instantiatesCorrectly_true() {
         Hero hero = new Hero("Superman");
         assertEquals(true, hero instanceof Hero);
+    }
+    @Test 
+    public void hero_instantiatesWithName_String() {
+        Hero hero = new Hero("Superman");
+        assertEquals("Superman", hero.getName());
     }
 }
