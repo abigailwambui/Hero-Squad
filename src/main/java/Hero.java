b25106940ee7008a9ecbaa69e76166ffa873e1f7
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-17 15:42:25 +0300

    pass test to initiate hero superpower

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,15 +1,20 @@
 public class Hero{
     private String mName;
     private Integer mAge;
+    private String mPower;
 
-    public Hero(String name, Integer age) {
+    public Hero(String name, Integer age, String power) {
         mName = name;
         mAge = age;
+        mPower = power;
     }
     public String getName(){
         return mName;
     }
     public Integer getAge(){
         return mAge;
+    }
+    public String getPower(){
+        return mPower;
     }
 }
--- src/test/HeroTest.java
+++ src/test/HeroTest.java
@@ -4,17 +4,22 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test 
     public void hero_instantiatesCorrectly_true() {
-        Hero hero = new Hero("Superman","30");
+        Hero hero = new Hero("Superman","30", "Extreme might");
         assertEquals(true, hero instanceof Hero);
     }
     @Test 
     public void hero_instantiatesWithName_String() {
-        Hero hero = new Hero("Superman","30");
+        Hero hero = new Hero("Superman","30", "Extreme might");
         assertEquals("Superman", hero.getName());
     }
     @Test 
     public void hero_instantiatesWithAge_Integer() {
-        Hero hero = new Hero("Superman", 30);
+        Hero hero = new Hero("Superman", 30, "Extreme might");
         assertEquals("Superman", hero.getAge());
+    }
+    @Test 
+    public void hero_instantiatesWithPower_String() {
+        Hero hero = new Hero("Superman", 30, "Extreme might");
+        assertEquals("Superman", hero.getPower());
     }
 }
