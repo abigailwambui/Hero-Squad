bc8eb77729e6280445baa507bc30a753455ab9d1
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-17 15:37:03 +0300

    pass test initiated hero age

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,10 +1,15 @@
 public class Hero{
     private String mName;
+    private Integer mAge;
 
-    public Hero(String name) {
+    public Hero(String name, Integer age) {
         mName = name;
+        mAge = age;
     }
     public String getName(){
         return mName;
+    }
+    public Integer getAge(){
+        return mAge;
     }
 }
--- src/test/HeroTest.java
+++ src/test/HeroTest.java
@@ -4,12 +4,17 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test 
     public void hero_instantiatesCorrectly_true() {
-        Hero hero = new Hero("Superman");
+        Hero hero = new Hero("Superman","30");
         assertEquals(true, hero instanceof Hero);
     }
     @Test 
     public void hero_instantiatesWithName_String() {
-        Hero hero = new Hero("Superman");
+        Hero hero = new Hero("Superman","30");
         assertEquals("Superman", hero.getName());
+    }
+    @Test 
+    public void hero_instantiatesWithAge_Integer() {
+        Hero hero = new Hero("Superman", 30);
+        assertEquals("Superman", hero.getAge());
     }
 }
