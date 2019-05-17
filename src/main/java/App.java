1908d13cd7a76856d12dad66ae4b3378368eca2b
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-17 17:36:01 +0300

    include an index.vtl and heroes.vtl

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+    
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("heroes/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/hero-form.vtl");
+            return new ModelAndView(model, layout);
+            }, new VelocityTemplateEngine());
+
+    
+    }
+}
