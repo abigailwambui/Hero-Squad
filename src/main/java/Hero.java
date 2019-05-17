30174f034e49e8aa56555c7e2ec944d732d10a46
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-17 15:48:45 +0300

    pass test to initiate hero weakness

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,11 +2,13 @@ public class Hero{
     private String mName;
     private Integer mAge;
     private String mPower;
+    private String mWeakness;
 
-    public Hero(String name, Integer age, String power) {
+    public Hero(String name, Integer age, String power, String weakness) {
         mName = name;
         mAge = age;
         mPower = power;
+        mWeakness = weakness;
     }
     public String getName(){
         return mName;
@@ -16,5 +18,8 @@ public class Hero{
     }
     public String getPower(){
         return mPower;
+    }
+    public String getWeakness(){
+        return mWeakness;
     }
 }
--- src/test/HeroTest.java
+++ src/test/HeroTest.java
@@ -4,22 +4,27 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test 
     public void hero_instantiatesCorrectly_true() {
-        Hero hero = new Hero("Superman","30", "Extreme might");
+        Hero hero = new Hero("Superman","30", "Extreme might","Ladies");
         assertEquals(true, hero instanceof Hero);
     }
     @Test 
     public void hero_instantiatesWithName_String() {
-        Hero hero = new Hero("Superman","30", "Extreme might");
+        Hero hero = new Hero("Superman","30", "Extreme might","Ladies");
         assertEquals("Superman", hero.getName());
     }
     @Test 
     public void hero_instantiatesWithAge_Integer() {
-        Hero hero = new Hero("Superman", 30, "Extreme might");
-        assertEquals("Superman", hero.getAge());
+        Hero hero = new Hero("Superman", 30, "Extreme might","Ladies");
+        assertEquals(30, hero.getAge());
     }
     @Test 
     public void hero_instantiatesWithPower_String() {
-        Hero hero = new Hero("Superman", 30, "Extreme might");
-        assertEquals("Superman", hero.getPower());
+        Hero hero = new Hero("Superman", 30, "Extreme might","Ladies");
+        assertEquals("Extreme might", hero.getPower());
+    }
+     @Test 
+    public void hero_instantiatesWithWeakness_String() {
+        Hero hero = new Hero("Superman", 30, "Extreme might","Ladies");
+        assertEquals("Ladies", hero.getWeakness());
     }
 }
