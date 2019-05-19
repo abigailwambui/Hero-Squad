60f9999faec632427d9359e854ae32396b018dea
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-19 20:22:46 +0300

    fail and pass tests to add id to my app

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -6,12 +6,16 @@ public class Squad {
     private String mSize;
     private String mCause;
     private static List<Squad> instances = new ArrayList<Squad>();
+    private int mId;
+    private List<Hero>mHeroes;
 
     public Squad(String name, String size, String cause) {
         mName = name;
         mSize = size;
         mCause = cause;
         instances.add(this);
+        mId = instances.size();
+        mHeroes = new ArrayList<Hero>();
 }
 
 public String getName(){
@@ -28,5 +32,22 @@ public static List<Squad> all() {
 }
 public static void clear() {
         instances.clear();
+}
+public int getId() {
+        return mId;
+}
+public static Squad find(int id) {
+        try {
+            return instances.get(id - 1);
+        } catch (IndexOutOfBoundsException exception) {
+    return null;
+        }
     }
+public List<Hero> getHeroes() {
+        return mHeroes;
+    }
+
+public void addHero(Hero hero) {
+        mHeroes.add(hero);
+    }    
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -36,4 +36,35 @@ public class SquadTest {
         Squad.clear();
         assertEquals(Squad.all().size(), 0);
     }
+    @Test 
+    public void getId_instantiateSquadsWithAnId_1() {
+        Squad squad = new Squad("PowerPuff Girls", "30", "Fight for Equality");
+        assertEquals(1, squad.getId());
+    }
+    @Test 
+    public void find_returnsSquadWithSameId_secondSquad() {
+        Squad.clear();
+        Squad firstSquad = new Squad("PowerPuff Girls", "30", "Fight for Equality");
+        Squad secondSquad = new Squad("Famous Five", "5", "Speak up for Children Rights");
+        assertEquals(Squad.find(secondSquad.getId()), secondSquad);
+    }
+     @Test 
+    public void getHeroes_initiallyReturnsEmptyList_ArrayList() {
+        Squad.clear();
+        Squad squad = new Squad("PowerPuff Girls", "30", "Fight for Equality");
+        assertEquals(0, squad.getHeroes().size());
+    }
+
+    @Test 
+    public void addHero_addsHeroToList_true() {
+        Squad squad = new Squad("PowerPuff Girls", "30", "Fight for Equality");
+        Hero hero = new Hero("Superman",30, "Extreme might","Ladies");
+        squad.addHero(hero);
+        assertTrue(squad.getHeroes().contains(hero));
+    }
+
+    @Test 
+    public void find_returnsNullWhenNoHeroFound_null() {
+        assertTrue(Squad.find(999) == null);
+    }
 }
