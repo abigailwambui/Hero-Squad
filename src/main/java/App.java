b5be94c05bcdefbb12f338c591ec1a2249e80af9
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-20 14:20:25 +0300

    add port to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,12 +11,14 @@ public class App {
         String layout = "templates/layout.vtl";
        
         ProcessBuilder process = new ProcessBuilder();
-     Integer port;
-     if (process.environment().get("PORT") != null) {
-         port = Integer.parseInt(process.environment().get("PORT"));
-     } else {
-         port = 4567;
-     }
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
     
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
